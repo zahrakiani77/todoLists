6bb68626dd06ca7245a8ea450dac690e768d684d
ZahraKiani <zahra.kiani7776@gmail.com>
2025-09-02 17:10:26 +0330

    feat: :sparkles: impliment create a task ui and tocken

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,16 +1,36 @@
 import type React from "react"
 import { useTheme } from "./stores/ThemeProvider";
+import { useEffect, useState } from "react";
 
 type layoutProp={
     children:React.ReactNode
 }
 const Layout = ({children}:layoutProp) => {
      const { darkMode } = useTheme();
-     console.log(darkMode)
+       const [isMobile, setIsMobile] = useState(false);
+
+       useEffect(() => {
+         const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+         const handleChange = () => setIsMobile(mediaQuery.matches);
+         handleChange(); 
+
+         mediaQuery.addEventListener("change", handleChange);
+         return () => mediaQuery.removeEventListener("change", handleChange);
+       }, []);
+
+       const bgImage = isMobile
+         ? darkMode
+           ? "/bg-mobile-dark.jpg"
+           : "/bg-mobile-light.jpg"
+         : darkMode
+           ? "/bg-desktop-dark.jpg"
+           : "/bg-desktop-light.jpg";
+
   return (
-    <div className="bg-background w-screen">
+    <div className="bg-background w-screen ">
       <img
-        src={`../public/${darkMode ? `bg-desktop-dark.jpg` : `bg-desktop-light.jpg`}`}
+        src={`../public${bgImage}`}
         className="absolute top-0 w-full"
       ></img>
       <div className="">{children}</div>
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,19 +4,34 @@ import { useTheme } from "../stores/ThemeProvider";
 const Container = () => {
       const { darkMode, toggleTheme} = useTheme();
   return (
-    <div className="flex w-full flex-row justify-center">
-      <div className="absolute top-1/5 z-50 flex w-10/12 border md:w-1/2">
+    <div className="flex w-full justify-center">
+      <div className="absolute top-1/5 z-50 flex w-10/12 flex-col gap-10 md:w-2/3 lg:w-1/2">
         <header className="flex w-full flex-row content-between justify-between">
-          <h1 className="text-white text-shadow-lg/30">To Do</h1>
-          <div className="m-2" onClick={toggleTheme}>
+          <h1 className="text-3xl text-white text-shadow-lg/30 md:text-3xl lg:text-5xl">
+            ToDo
+          </h1>
+          <div className="my-2 cursor-pointer" onClick={toggleTheme}>
             {darkMode ? (
-            <Sun className="text-white" size={32} />
+              <Sun className="text-white md:size-8" />
             ) : (
-              <Moon className="text-white" size={32} />
-
+              <Moon className="text-white md:size-8" />
             )}
           </div>
         </header>
+        <section>
+          <label className="input md:input-xl w-full">
+            <input
+              type="radio"
+              name="radio-4"
+              className="radio radio-primary"
+            />
+            <input
+              type="text"
+              placeholder="create a new todo..."
+              className="input input-primary md:input-lg"
+            />
+          </label>
+        </section>
       </div>
     </div>
   );
